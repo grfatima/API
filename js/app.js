56c33a640f03c8a39373090f6624a93ab527f9d6
grfatime <kurbanovhafatime@gmail.com>
2025-04-29 23:39:47 +0400

    api

--- js/app.js
+++ js/app.js
@@ -2,27 +2,56 @@ const ENDPOINT = "http://localhost:3000";
 const uniCards = document.querySelector("#uniCards");
 const ixtisasCards = document.querySelector("#ixtisasCards");
 
-const getUniversitetler = () => {
-  axios.get(ENDPOINT + "/university").then(({ data }) => {
-    data.forEach(({ id, code, name }) => {
-      uniCards.innerHTML += ` <div class="card">
-          <h2>${code}</h2>
-          <p>${name}</p>
-        </div>`;
-    });
-  });
-};
-getUniversitetler();
-
-const getIxtisaslar = () => {
-  axios.get(ENDPOINT + "/ixtisaslar").then(({ data }) => {
-    data.forEach(({ id, code, name }) => {
-      ixtisasCards.innerHTML += `  <div class="card">
-          <h2>${code}</h2>
-          <p>${name}</p>
-        </div>`;
-    });
+const showData = (url, parent) => {
+  axios.get(ENDPOINT + url).then(({ data }) => {
+    if (data && data.length > 0) {
+      data.forEach(({ id, code, name }) => {
+        parent.innerHTML += ` <div class="card">
+               <div> 
+               <h2>${code}</h2>
+                <p>${name}</p>
+               </div>
+               <div><i class="fa-solid fa-trash trash"></i>
+               <i class="fa-solid fa-file-pen pen"></i></div>
+              </div>`;
+      });
+    } else {
+      parent.innerHTML = "Universitet siyahisi tapilmadi.";
+    }
   });
 };
 
-getIxtisaslar();
+showData("/university", uniCards);
+showData("/ixtisaslar", ixtisasCards);
+
+// const getUniversitetler = () => {
+//   axios.get(ENDPOINT + "/university").then(({ data }) => {
+//     if (data && data.length > 0) {
+//       data.forEach(({ id, code, name }) => {
+//         uniCards.innerHTML += ` <div class="card">
+//             <h2>${code}</h2>
+//             <p>${name}</p>
+//           </div>`;
+//       });
+//     } else {
+//       uniCards.innerHTML = "Universitet siyahisi tapilmadi.";
+//     }
+//   });
+// };
+// getUniversitetler();
+
+// const getIxtisaslar = () => {
+//   axios.get(ENDPOINT + "/ixtisaslar").then(({ data }) => {
+//     if (data && data.length) {
+//       data.forEach(({ id, code, name }) => {
+//         ixtisasCards.innerHTML += `  <div class="card">
+//                 <h2>${code}</h2>
+//                 <p>${name}</p>
+//               </div>`;
+//       });
+//     } else {
+//       ixtisasCards.innerHTML = "Ixtisas siyahisi tapilmadi.";
+//     }
+//   });
+// };
+// getIxtisaslar();
